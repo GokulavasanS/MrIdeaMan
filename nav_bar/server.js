63712bf781b411efc27031a2f4530ad7dedcf4d7
Gokulavasan S <mrideaman03@gmail.com>
2024-02-04 19:09:17 +0530

    new modification

--- nav_bar/server.js
+++ nav_bar/server.js
@@ -38,7 +38,7 @@ app.get('/contact', (req, res) => {
 });
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3003;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
